0ba51a05cb171a501c0aba075130e5ab23e80ce4
henu0307 <hyun135@gmail.com>
2023-05-12 19:56:44 +0900

    지도 시작 좌표 조정, 누비자 터미널 추가(1개), 지도 속성 추가

--- app/src/main/java/com/example/nubijatrip/MainActivity.kt
+++ app/src/main/java/com/example/nubijatrip/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.nubijatrip
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.UiThread
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Transformations.map
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 
-class MapFragmentActivity : FragmentActivity(), OnMapReadyCallback {
+class MainActivity : FragmentActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,6 +30,20 @@ class MapFragmentActivity : FragmentActivity(), OnMapReadyCallback {
 
     @UiThread
     override fun onMapReady(naverMap: NaverMap) {
-        // ...
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, true)
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isCompassEnabled = true
+        uiSettings.isScaleBarEnabled = true
+
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+        val marker = Marker()
+        marker.position = LatLng(35.21663989, 128.6942419)
+        marker.captionText = "성원중앙상가"
+        marker.captionColor = Color.BLUE
+        marker.captionTextSize = 16f
+        marker.isHideCollidedSymbols = true
+        marker.isHideCollidedCaptions = true
+        marker.map = naverMap
     }
 }
