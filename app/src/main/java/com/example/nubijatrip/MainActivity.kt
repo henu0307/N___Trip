e95185a12a0f2fc2ab15c839513b1d5aef0af3b7
henu0307 <hyun135@gmail.com>
2023-05-12 19:48:59 +0900

    NaverMap 객체를 얻어오기 위한 코드 추가

--- app/src/main/java/com/example/nubijatrip/MainActivity.kt
+++ app/src/main/java/com/example/nubijatrip/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.nubijatrip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.UiThread
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Transformations.map
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MapFragmentActivity : FragmentActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
+    }
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        // ...
     }
 }
